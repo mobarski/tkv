985679863c2859ead2e42788a36dcb99c12efae1
Maciej Obarski <mobarski@gmail.com>
2022-06-09 23:27:26 +0200

    sqlite view

--- tkv_sqlite.py
+++ tkv_sqlite.py
@@ -146,6 +146,7 @@ class TKVsqlite(tkv.TKV):
 		self._execute(sql)
 
 
+
 class TKVsqlitetable(tkv.TKV):
 	
 	def __init__(self, path=':memory:', table='tkv', dumps=None, loads=None, **kw):
@@ -266,8 +267,80 @@ class TKVsqlitetable(tkv.TKV):
 		self._execute(sql)
 
 
+
+class TKVsqliteview(tkv.VTKV):
+	
+	def __init__(self, path, **kw):
+		self.db = sqlite3.connect(path, **kw)
+		self.sep_tab = '/'
+		self.sep_col = ','
+		self.dumps = None # NOT USED - required for self.table
+		self.loads = None # NOT USED - required for self.table
+	
+	def get(self, tab, key, default=None):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		sql = f'select "{db_col}" from "{db_tab}" where "{db_key}"=?'
+		return self._execute(sql, (key,)).fetchone()[0] # TODO
+
+	def has(self, tab, key):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		sql = f'select "{db_key}" from "{db_tab}" where "{db_key}"=?'
+		return bool(self._execute(sql, (key,)).fetchone())
+
+	def size(self, tab):
+		not_implemented(self, 'size')
+
+	# iterators
+	
+	def keys(self, tab, sort=False):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		sql = f'select "{db_col}" from "{db_tab}" order by "{db_key}"'
+		return self._execute(sql) # TODO
+	
+	# TODO: multicolumn support
+	def items(self, tab, sort=False):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		sql = f'select "{db_key}","{db_col}" from "{db_tab}" order by "{db_key}"'
+		return self._execute(sql) # TODO
+
+	# extension
+	
+	def get_many(self, tab, keys, default=None):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		placeholders = self.sep_col.join(['?']*len(keys))
+		sql = f'select "{db_key}","{db_col}" from "{db_tab}" where "{db_key}" in ({placeholders})'
+		if self.sep_col in db_col: # support for multicolumn values
+			resp = {x[0]:x[1:] for x in self._execute(sql,keys)}
+		else:
+			resp = dict(self._execute(sql,keys))
+		return [resp[k] if k in resp else default for k in keys]
+	
+	def count(self, tab):
+		db_tab, db_key, db_col = self._parse_tab(tab)
+		sql = f'select count(*) from "{db_tab}"'
+		return self._execute(sql).fetchone()[0] # TODO
+
+	# internal
+	
+	def _parse_tab(self, tab):
+		tab,key,col = tab.upper().split(self.sep_tab)[:3]
+		if self.sep_col in col: # support for multicolumn values
+			col = col.replace(self.sep_col,f'"{self.sep_col}"')
+		return tab,key,col
+		
+	def _execute(self, sql, *a):
+		print('SQL>',sql) # xxx
+		results = self.db.execute(sql,*a)
+		# columns = [x.name.lower() for x in cur.description]
+		return results
+
+
+
 def connect(*a,**kw):
 	return TKVsqlite(*a,**kw)
 
 def connect_table(*a,**kw):
 	return TKVsqlitetable(*a,**kw)
+
+def connect_view(*a,**kw):
+	return TKVsqliteview(*a,**kw)
